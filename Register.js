bc4dce199c34fcea1d8eb2451c149ad0d5b523b0
Abraham Ngom <43425508+falkune@users.noreply.github.com>
2021-06-23 17:06:46 +0200

    modif last

--- Register.js
+++ Register.js
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 import firebase from 'firebase';
 
 function Register(){
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const hadleUsername = e => {
+    setUserName(e)
+  }
+
+  const handleEmail = e => {
+    setEmail(e)
+  }
+
+  const handlePassword = e => {
+    setPassword(e)
+  }
+
+  var db = firebase.firestore();
+
+  const handleSubmit = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      db.collection("users").doc(email).set({
+        userName: userName,
+        password: password
+      })
+      .then(() => {
+          console.log("Document successfully written!");
+      })
+      .catch((error) => {
+          console.error("Error writing document: ", error);
+      });
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage)
+    });
+  }
+
   return(
     <View style={styles.container2}>
       <Text style={styles.barstyle}>Bienveune dans Register</Text>
-      <TextInput style={styles.input} placeholder="user name"/>
-      <TextInput style={styles.input} placeholder="email"/>
-      <TextInput style={styles.input} type="password" placeholder="password"/>
+      <TextInput 
+        style={styles.input} 
+        placeholder="user name"
+        onChangeText={hadleUsername}/>
+      <TextInput 
+        style={styles.input} 
+        placeholder="email"
+        onChangeText={handleEmail}/>
+      <TextInput 
+        style={styles.input} 
+        type="password" 
+        placeholder="password"
+        onChangeText={handlePassword}/>
       <View style={styles.buttonContainer}>
-        <Button title="INSCRIPTION"/>
+        <Button 
+          title="INSCRIPTION"
+          onPress={handleSubmit}/>
       </View>
     </View>
   )
